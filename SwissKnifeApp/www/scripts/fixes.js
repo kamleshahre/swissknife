e8c331ca255ec6f0fda1fb9de056a13de58d5bd1
Nico Verbruggen <nico.verb@gmail.com>
2013-12-09 13:29:06 +0100

    More fixes, will merge after this

--- SwissKnifeApp/www/scripts/fixes.js
+++ SwissKnifeApp/www/scripts/fixes.js
@@ -4,8 +4,10 @@
  $(document).ready(function(){
      $("input").focus(function() {
         $("nav").hide();
+        $("body").css("padding-top", "15px");
      });
      $("input").blur(function() {
         $("nav").show();
+        $("body").css("padding-top", "0px");
      });
  });
