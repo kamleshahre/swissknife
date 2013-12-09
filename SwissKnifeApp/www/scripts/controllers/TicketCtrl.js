15f9c9167bb0578b4efe990cfa7448620654a85c
Nico Verbruggen <nico.verb@gmail.com>
2013-12-09 16:29:58 +0100

    Tickets!

--- SwissKnifeApp/www/scripts/controllers/TicketCtrl.js
+++ SwissKnifeApp/www/scripts/controllers/TicketCtrl.js
@@ -16,7 +16,7 @@
         $scope.ticket = {
             "username" : "nicoverbruggen",
             "event" : "Festivalitis",
-            "ticket_id" : 535607621,
+            "ticket_id" : "D0D2AAD534EBEC8B37B855CA91FBFB1A3B4B251E17B7CAB383C4A484718664720769188E1E78DE05E313726AF0D7AB0F2D968F07ACA8F4960D8C05BEDD43E347",
             "email" : "nico.verb@gmail.com",
             "price" : "25 EUR"
         };
