d6fc9b680b14633677f5f9e52b87f7597345a3e7
Van Acker Bjorn <bjornvanacker8@hotmail.com>
2013-12-27 19:01:49 +0100

    Register via POST works

--- SwissKnifeApp/www/scripts/controllers/ProfileCtrl.js
+++ SwissKnifeApp/www/scripts/controllers/ProfileCtrl.js
@@ -5,10 +5,18 @@
     var controllers = angular.module('swissKnifeApp.controllers');
     // SET MAIN CONTROLLER
     controllers.controller('swissKnifeApp.controllers.ProfileCtrl',['$scope','$rootScope', function($scope, $rootScope){
-        $scope.lflplace = {
-            lat:$rootScope.user['tent']['location']['location_lat'],
-            lng:$rootScope.user['tent']['location']['location_long'],
-            dsc:'<strong>' + $rootScope.user['user_username'] + '\'s tent</strong>'
-        };
+        if($rootScope.user['tent'] !== null){
+            $scope.lflplace = {
+                lat:$rootScope.user['tent']['location']['location_lat'],
+                lng:$rootScope.user['tent']['location']['location_long'],
+                dsc:'<strong>' + $rootScope.user['user_username'] + '\'s tent</strong>'
+            };
+        }else{
+            $scope.lflplace = {
+                lat:51.086849,
+                lng:3.669939,
+                dsc:'<strong>' + $scope.user['user_username'] + '\'s tent is not set</strong>'
+            };
+        }
     }]);
 })();
--- SwissKnifeApp/www/scripts/controllers/RegisterCtrl.js
+++ SwissKnifeApp/www/scripts/controllers/RegisterCtrl.js
@@ -9,6 +9,39 @@
     // SET UP CONTROLLERS AS ANGULAR MODULE
     var controllers = angular.module('swissKnifeApp.controllers');
     // SET MAIN CONTROLLER
-    controllers.controller('swissKnifeApp.controllers.RegisterCtrl',['$scope', function($scope){
+    controllers.controller('swissKnifeApp.controllers.RegisterCtrl',['$scope', '$rootScope', '$http', '$location', function($scope, $rootScope, $http, $location){
+        $scope.DoRegisterAction = function(){
+            $scope.isBusy = true;
+            $scope.isAlert = false;
+            console.log("register?");
+            var data = {"email" : $scope.email, "password" : $scope.password,"username" : $scope.username};
+            $http.post('http://localhost/HybridAPI/public/API/user/create', data)
+                .success(function(returned_data){
+                    if (returned_data.status !== ""){
+                        // Fix the navigation glitch
+                        $("nav").show();
+                        $location.path( "/login" );
+                    }else{
+                        console.log = "I have no idea.";
+                    }
+                    $scope.isBusy = false;
+                })
+                .error(function(data, status, headers, config){
+                    // This will usually be called when we have no internets.
+                    console.log(status);
+                    if (status === 401){
+                        $scope.alertMessage = data.error;
+                    }
+                    else if(status === 0){
+                        console.log(data + status + headers + config);
+                        $scope.alertMessage = "You seem to be offline.";
+                    }else{
+                        $scope.alertMessage = "Oops. Something went wrong. Please contact our space gnomes with the following information: <info>STATUS: " + status + " / HEADERS: " + headers + " / CONFIG: " + config + ".</info>";
+                    }
+                    // Show alert message
+                    $scope.isAlert = true;
+                    $scope.isBusy = false;
+                });
+        };
     }]);
 })();
