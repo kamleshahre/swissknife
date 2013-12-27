00d43b4438eb78f325a801b9e2015c29e33c33e6
Van Acker Bjorn <bjornvanacker8@hotmail.com>
2013-12-27 20:01:10 +0100

    Ticket fix (no QR ?)

--- SwissKnifeApp/www/scripts/controllers/TicketCtrl.js
+++ SwissKnifeApp/www/scripts/controllers/TicketCtrl.js
@@ -13,15 +13,20 @@
     // Defining the controller
     controllers.controller('swissKnifeApp.controllers.TicketCtrl',['$scope','$rootScope', function($scope,$rootScope){
 
-        $scope.ticket = {
-            "username" :$rootScope.user["user_username"],
-            "event" : "Festivalitis",
-            "ticket_id" : $rootScope.user["ticket"]["ticket_body"],
-            "email" : $rootScope.user["user_mail"],
-            "price" : "25 EUR"
-        };
-        
-        $('#qrcode').qrcode($scope.ticket["username"] + "|" + $scope.ticket["ticket_id"]);
+        if($rootScope.user["ticket"] !== null){
+            console.log($rootScope.user["ticket"]["ticket_body"]);
+            $scope.ticket = {
+                "username" :$rootScope.user["user_username"],
+                "event" : "Festivalitis",
+                "ticket_id" : $rootScope.user["ticket"]["ticket_body"],
+                "email" : $rootScope.user["user_mail"],
+                "price" : "25 EUR"
+            };
+
+            $('#qrcode').qrcode($scope.ticket["username"] + "|" + $scope.ticket["ticket_id"]);
+        }else{
+            $scope.ticket = null;
+        }
             
     }]);
 })();
